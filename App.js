e3e4010cb2e50ab0e946bba1a67114bcd890438f
ali7184 <alimurtuza718@gmail.com>
2019-08-18 07:12:04 -0700

    Header created

--- App.js
+++ App.js
@@ -1,26 +1,68 @@
 import React, { Component } from "react";
+import { Container, Header, Tab, Tabs, TabHeading, Icon } from "native-base";
 import { Text, View } from "react-native";
-import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import Feed from "./app/screens/feed";
-import Profile from "./app/screens/profile";
 import Upload from "./app/screens/upload";
-
-const Navigator = createBottomTabNavigator({
-  Feed: {
-    screen: Feed
-  },
-  Profile: {
-    screen: Profile
-  },
-  Upload: {
-    screen: Upload
-  }
-});
-
-const MainStack = createAppContainer(Navigator);
+import Profile from "./app/screens/profile";
 
 export default class App extends Component {
+  state = {
+    activeTab: 0
+  };
   render() {
-    return <MainStack />;
+    return (
+      <View style={{ flex: 1 }}>
+        <Tabs
+          onChangeTab={val => this.setState({ activeTab: val.i })}
+          tabBarUnderlineStyle={{ opacity: 0 }}
+          tabBarPosition="bottom"
+        >
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "white" }}>
+                <Icon
+                  name="home"
+                  style={{
+                    color: this.state.activeTab === 0 ? "black" : "gray"
+                  }}
+                />
+              </TabHeading>
+            }
+          >
+            <Feed />
+          </Tab>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "white" }}>
+                <Icon
+                  name="plus-square"
+                  type="FontAwesome5"
+                  style={{
+                    color: this.state.activeTab === 1 ? "black" : "gray"
+                  }}
+                />
+              </TabHeading>
+            }
+          >
+            <Upload />
+          </Tab>
+          <Tab
+            heading={
+              <TabHeading style={{ backgroundColor: "white" }}>
+                <Icon
+                  name="user"
+                  type="FontAwesome5"
+                  style={{
+                    color: this.state.activeTab === 2 ? "black" : "gray"
+                  }}
+                />
+              </TabHeading>
+            }
+          >
+            <Profile />
+          </Tab>
+        </Tabs>
+      </View>
+    );
   }
 }
--- app/screens/feed.js
+++ app/screens/feed.js
@@ -1,12 +1,74 @@
 import React, { Component } from "react";
-import { Text, View, SafeAreaView, StyleSheet, Platform } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image
+} from "react-native";
 import styles from "../../constants/styles";
+import { Icon } from "native-base";
+import logo from "../../assets/Images/instagram.png";
 
 export default class Feed extends Component {
   render() {
     return (
       <SafeAreaView style={styles.SafeArea}>
-        <Text> Feed page </Text>
+        <View style={{ width: "100%", flex: 1 }}>
+          {/* Header */}
+          <View
+            style={{
+              width: "100%",
+              height: 50,
+              backgroundColor: "white",
+              elevation: 3
+            }}
+          >
+            <View style={{ width: "100%", flex: 1, flexDirection: "row" }}>
+              <View
+                style={{
+                  width: "50%",
+                  flex: 1,
+                  justifyContent: "flex-start",
+                  flexDirection: "row",
+                  alignItems: "center"
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "30%"
+                  }}
+                >
+                  <Icon name="camera" type="Feather" />
+                </TouchableOpacity>
+                <Image source={logo} style={{ width: 100, height: 30 }} />
+              </View>
+              <View
+                style={{
+                  width: "50%",
+                  flex: 1,
+                  justifyContent: "flex-end",
+                  flexDirection: "row",
+                  alignItems: "center"
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "30%"
+                  }}
+                >
+                  <Icon name="paper-plane" type="SimpleLineIcons" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          {/* Header ends */}
+        </View>
       </SafeAreaView>
     );
   }
